a5307026d500ec503cbdf474d7084431df55f7b3
Jennifer Quach <jenniq3@uci.edu>
2024-02-22 21:44:16 -0800

    [Backend] Add activity and update api

--- server/app.py
+++ server/app.py
@@ -4,13 +4,13 @@ from flask import Flask, request
 from waitress import serve
 from apis.register import register_user
 from apis.signin import signin_user
-import requests
 import sys
 from datetime import datetime, timedelta
 from apis import sleep
-import apis.state as manager_instance
+from apis import exercise
+import apis.state as manage_instance
+import apis.update as update
 
-import apis.model as model
 import apis.utilities as utilities
 
 app = Flask(__name__)
@@ -26,7 +26,7 @@ def v1register():
 
     data_retrieved_register = request.data
     decoded_data_register = data_retrieved_register.decode('utf-8')
-    instance = manager_instance.ProfileManager.instance()
+    instance = manage_instance.ProfileManager.instance()
     return register_user(authentication, decoded_data_register, instance)
 
 
@@ -38,39 +38,57 @@ def v1signIn():
 
     data_retrieved_sign_in = request.data
     decoded_data_sign_in = data_retrieved_sign_in.decode('utf-8')
-    instance = manager_instance.ProfileManager.instance()
+    instance = manage_instance.ProfileManager.instance()
     return signin_user(authentication, decoded_data_sign_in, instance)
 
 
 @app.route("/v1/getSleepRecommendation", methods=['GET'])
 def v1sleep_recommendation():
-    authentication, status = utilities.check_authorization(request.headers)
+    instance = manage_instance.ProfileManager.instance()
+    email, status = utilities.check_authentication_login(request.headers, instance)
     if status != 200:
-        return authentication, status
-
-    email, password = utilities.get_email_password(authentication)
-
-    instance = manager_instance.ProfileManager.instance()
-    check_email = utilities.check_email_password(email, password, instance, 1)
-    if check_email == 401:
-        return {'error_message': 'Incorrect email or password'}, 401
+        return email, status
 
     sleep_args = request.args
-
     _, user = instance.get_user(email, 1)
     user.update_location((sleep_args.get("location_lat"), sleep_args.get("location_long")))
 
     date = sleep_args.get('date')
     if date is None:
         date = (datetime.now() - timedelta(hours=6)).isoformat()
-    return sleep.get_sleep_recommendation(email, date)
+    return sleep.get_sleep_recommendation(email, date), 200
 
 
 @app.route("/v1/getFoodRecommendation", methods=['GET'])
-def food_recommendation():
-    authentication = request.headers["Authorization"]
-    status, email, password = utilities.create_email_password(authentication)
+def v1food_recommendation():
     return "Hello, World!"
+
+
+@app.route("/v1/getActivityRecommendation", methods=['GET'])
+def v1activity_recommendation():
+    instance = manage_instance.ProfileManager.instance()
+    email, status = utilities.check_authentication_login(request.headers, instance)
+    if status != 200:
+        return email, status
+
+    exercise_args = request.args
+    _, user = instance.get_user(email, 1)
+    user.update_location((exercise_args.get("location_lat"), exercise_args.get("location_long")))
+
+    return exercise.get_activity_recommendation(email, datetime.now().isoformat())
+
+
+@app.route("/v1/updateHealthData", methods=['POST'])
+def v1update_health_data():
+    authentication, status = utilities.check_authorization(request.headers)
+    if status != 200:
+        return authentication, status
+
+    data_retrieved_update_data = request.data
+    decoded_data_update_data = data_retrieved_update_data.decode('utf-8')
+    instance = manage_instance.ProfileManager.instance()
+
+    return update.update_health_data(authentication, decoded_data_update_data, instance)
 
 
 mode = 'dev'

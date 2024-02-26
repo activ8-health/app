39f6cad0e9d714c1637cdbb8af762bf825dd923c
Vanessa Tang <vanesst5@uci.edu>
2024-02-25 21:59:23 -0800

    [Backend] Adding type annotations, docstrings, comments

--- server/apis/lifestyle.py
+++ server/apis/lifestyle.py
@@ -3,7 +3,14 @@ import sleep
 from dateutil import parser
 
 
-def get_user_data(email):
+def get_user_data(email: str) -> dict:
+    '''
+    email: email of the user to retrieve user data from
+    returns necessary user data
+
+    opens and loads the user data from json file 
+    retrieves the user's exercise data, sleep data, and food data to use for calculations
+    '''
     user_file = open('./data/user_profile.json', 'r')
     user_data = json.load(user_file)
     exercise = user_data[email]['exercise']
@@ -11,10 +18,19 @@ def get_user_data(email):
     food = user_data[email]['food']
     return {'sleep': sleep, 'exercise': exercise, 'food': food}
 
-def calc_sleep_score(sleep_data):
+def calc_sleep_score(sleep_data: dict) -> int:
+    '''
+    sleep_data: a dictionary for list of sleep data points for each day of the week
+    returns the user's sleep score
+
+    a user's sleep score is calculated by getting the average sleep time for 7 days divided by the ideal sleep range
+    the score is maxed at 1
+    '''
     days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
     avg_sleep_time = 0
     for day in days:
+        # if there are data points that within 30 mins of each other, they are combined
+        # other than those cases, only one data point should be taken from each day
         i = 1
         date = sleep_data[day][0]
         while i < len(sleep_data[day]):
@@ -32,7 +48,14 @@ def calc_sleep_score(sleep_data):
     sleep_score = (avg_sleep_time / sleep.IDEAL_SLEEP_RANGE_IN_MINS)
     return min(sleep_score, 1)
 
-def calc_exercise_score(exercise_data):
+def calc_exercise_score(exercise_data: dict) -> int:
+    '''
+    exercise_data: a dictionary including the user's step data for each day and their step goal
+    returns the user's exercise score
+
+    a user's exercise score is calculated by getting the first 7 step data and averaging them before dividing them by the step goal
+    the score is maxed at 1
+    '''
     step_goal = exercise_data['step_goal']
     step_data = exercise_data['step_data']
     avg_steps = 0
@@ -42,7 +65,7 @@ def calc_exercise_score(exercise_data):
     exercise_score = avg_steps / step_goal
     return min(exercise_score, 1)
 
-def calc_food_score(food_data):
+def calc_food_score(food_data: dict) -> tuple[int, bool]:
     food_log = food_data['food_log']
     food_goal = food_data['weight_goal']
     avg_cals = 0
@@ -54,7 +77,15 @@ def calc_food_score(food_data):
     food_score = 0 #1 - (((avg_cals - ideal_cals) / 600) ** 2)
     return food_score, (avg_cals < ideal_cals)
 
-def get_lifestyle_score(email):
+def get_lifestyle_score(email: str) -> dict:
+    '''
+    email: email of the user to retrieve user data from
+    returns an object containing the user's lifestyle score and some advice to improve their lowest scoring category
+
+    calculates the sleep, exercise, and food score before average all of them
+    then converts the score to be out of 100 and returns a message based on the category with the lowest score
+    the food score related message changes based on whether the user exceeds their calorie goal or fails to meet it
+    '''
     user_data = get_user_data(email)
     sleep_score = calc_sleep_score(user_data['sleep'])
     print('sleep_score:', sleep_score)
